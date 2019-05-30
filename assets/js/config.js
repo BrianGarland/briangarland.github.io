fee81feb3b287a9e09a1780ab763d364878f1bbe
Brian Garland <brian.garland@vtinfo.com>
2019-05-30 12:19:24 -0400

    start over

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-var config = {
-    'api_server': '//localhost:9001/blog/'
-};
--- assets/js/views/home.js
+++
@@ -1,9 +0,0 @@
-views.home = function(data, params){
-    var api_stub = 'posts/index.json';
-    
-    utils.request(
-        api_stub,
-        'home_page',
-        'home_page_error'
-    );
-};
