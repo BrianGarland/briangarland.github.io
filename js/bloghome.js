83e1a60c43d1d569c6800de30e44bc3f40d46170
Brian Garland <brian.garland@vtinfo.com>
2019-10-28 15:25:56 -0400

    start adding blog elements

---
+++ js/bloghome.js
@@ -0,0 +1,42 @@
+
+var converter = new showdown.Converter();
+
+window.onload = function () { 
+  var issueID = Number(location.search.substr(1));
+
+  loadIssues(issueID);
+}
+
+function loadIssues(id) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.github.com/repos/briangarland/blog/issues", true);
+  xhr.send();
+  xhr.onload = function() {
+    var data = JSON.parse(this.responseText);
+
+    var posts = document.getElementById("posts");
+
+    var outhtml = '';
+
+    outhtml += `<p>Here is a list of my blog posts.</p>`
+
+    outhtml += '<ul>';
+    
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].user.login === "briangarland") {
+        outhtml += 
+        `<li><a href="ghpost.html?` + data[i].number + `">` + data[i].title +`</a>`
+
+        for (var y = 0; y < data[i].labels.length; y++) {
+          outhtml += ` <span class="badge badge-secondary" style="background-color: #` + data[i].labels[y].color + `;">` + data[i].labels[y].name + `</span>`
+        }
+
+        outhtml += `</li>`;
+      }
+    }
+
+    outhtml += '</ul>';
+
+    posts.innerHTML = outhtml;
+  }
+}
